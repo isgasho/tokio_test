e0260abfc09c0c14309bf829209a78b0dd345361
hevienz <hevienz@qq.com>
2018-07-12 20:35:41 +0800

    fmt code

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,54 @@
 #[macro_use]
 extern crate log;
+extern crate futures;
 extern crate pretty_env_logger;
 extern crate tokio;
-extern crate futures;
 
-
-use tokio::net::{TcpListener, TcpStream};
+use futures::future::{done, ok};
 use futures::{Future, Stream};
-use futures::future::{ok, done};
 use tokio::io::AsyncRead;
+use tokio::net::{TcpListener, TcpStream};
 
+use std::error;
 use std::fmt;
 use std::io;
-use std::error;
-
 
 fn client_fut(socket: TcpStream) -> impl Future<Item = (), Error = ()> + 'static + Send {
     futures::lazy(move || match socket.peer_addr() {
         Ok(peer) => {
             info!("Tcp connection [{:?}] connected to server", peer);
             Ok((socket, peer))
-        },
+        }
         Err(err) => {
             error!("Fetch peer address failed: {:?}", err);
             Err(())
         }
-    }).and_then(move |(socket, peer)| {
-        ok((socket.split(), peer))
-    }).and_then(move |((ref mut r, ref mut w), peer)| {
-        let loop_fut = done(io::copy(r, w)).and_then(move |num| {
-            info!("Wrote {:?} bytes", num);
-            ok(())
-        }).map_err(|err| {
-            error!("Write failed: {:?}", err);
-        });
+    }).and_then(move |(socket, peer)| ok((socket.split(), peer)))
+        .and_then(move |((ref mut r, ref mut w), peer)| {
+            let loop_fut = done(io::copy(r, w))
+                .and_then(move |num| {
+                    info!("Wrote {:?} bytes", num);
+                    ok(())
+                })
+                .map_err(|err| {
+                    error!("Write failed: {:?}", err);
+                });
 
-        tokio::spawn(loop_fut);
-        ok(())
-    })
+            tokio::spawn(loop_fut);
+            ok(())
+        })
 }
 
-fn server_fut(listener: TcpListener) -> impl Future<Item=(), Error = ()> + 'static + Send {
-    listener.incoming().for_each(|socket| {
-        tokio::spawn(client_fut(socket));
-        Ok(())
-    }).map_err(|err| {
-        error!("Accept connection failed: {:?}", err);
-    })
+fn server_fut(listener: TcpListener) -> impl Future<Item = (), Error = ()> + 'static + Send {
+    listener
+        .incoming()
+        .for_each(|socket| {
+            tokio::spawn(client_fut(socket));
+            Ok(())
+        })
+        .map_err(|err| {
+            error!("Accept connection failed: {:?}", err);
+        })
 }
 
 fn run() -> Result<(), io::Error> {
@@ -62,7 +64,7 @@ fn run() -> Result<(), io::Error> {
 
 fn print<T: fmt::Debug, E: error::Error>(result: Result<T, E>) {
     match result {
-        Ok(any)  => info!("Result: {:?}", any),
+        Ok(any) => info!("Result: {:?}", any),
         Err(err) => error!("Error: {:?}", err),
     }
 }
